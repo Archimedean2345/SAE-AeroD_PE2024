e124e19576aee9d3e699d93c1ff54cf5a8aafc6f
Leonardo VO <90436552+Archimedean2345@users.noreply.github.com>
2024-06-03 13:54:38 -0600

    Add files via upload

---
+++ pruebas.py
@@ -0,0 +1,31 @@
+import math
+a=1; b=2
+sum= a+b
+tangente= math.tan(45)
+print(math.tanh(45))
+# tanh es tangente hiperbolico
+print("tu suma es: ", sum)
+print("el tangente de 45 es:", tangente)
+print("Hello world")
+
+# Asi se hacen comentarios de linea
+
+"""
+Comentarios de un texto entero
+para hacerlos mas largos
+
+"""
+
+# Usar Matplotlib o Plotly 
+# Matplotlib es mas conveniente para figuras de buena resolucion en PDF
+# Plotly permite interactividad en el codigo
+
+# ---Usar pandas para importar informacion---
+"""
+La biblioteca pandas es una poderosa herramienta en Python para la manipulación y análisis de datos. 
+Proporciona estructuras de datos flexibles y expresivas que facilitan el trabajo con datos estructurados 
+(como tablas). 
+Error al instalar librerias solucionar con: https://www.youtube.com/watch?v=i1YuWtsRKsA&t=583s
+Funciona con con command CMD desde windows
+"""
+# ---Pruebas de funciones matematicas
